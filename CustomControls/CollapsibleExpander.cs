e7bfa3f1a64f6e723b62a490e5a681c5db0b81ea
Ravi Singh <rv_eps@hotmail.com>
2019-01-05 11:34:25 +0530

    Update CollapsibleExpander.cs

--- CustomControls/CollapsibleExpander.cs
+++ CustomControls/CollapsibleExpander.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 namespace RSPLMarketSurvey.CustomControls
 {
@@ -14,13 +15,20 @@ namespace RSPLMarketSurvey.CustomControls
         private Color _titleTextColor = Color.White;
         private FontAttributes _titleFontAttributes = FontAttributes.Bold;
         private Thickness _contentPadding = 5;
-        private bool _displayOnlySingleContent=true;
-        StackLayout mainContainer =null;
+        private bool _displayOnlySingleContent = true;
+        StackLayout mainContainer = null;
+        private bool _animating = false;
+
         public CollapsibleExpander()
         {
 
             ExpandableItems = new List<ExpandableItem>();
 
+        }
+        public enum AnimationEnum
+        {
+            None = 0,
+            Resize = 1,
         }
 
         protected override void OnParentSet()
@@ -39,106 +47,134 @@ namespace RSPLMarketSurvey.CustomControls
                 int itemIndex = 0;
                 foreach (var item in ExpandableItems)
                 {
-                    StackLayout subContainer = new StackLayout { HorizontalOptions = LayoutOptions.FillAndExpand };
+
+                    StackLayout subContainer = new StackLayout { HorizontalOptions = LayoutOptions.FillAndExpand, IsClippedToBounds = true };
                     StackLayout titleBar = new StackLayout { StyleId = "titlebar_" + itemIndex.ToString(), HeightRequest = TitleHeight, BackgroundColor = TitleBackColor, Orientation = StackOrientation.Horizontal, HorizontalOptions = LayoutOptions.FillAndExpand };
                     TapGestureRecognizer titleBar_TapGestureRecognizer = new TapGestureRecognizer();
                     titleBar_TapGestureRecognizer.Tapped += TitleBar_TapGestureRecognizer_Tapped;
                     titleBar.GestureRecognizers.Add(titleBar_TapGestureRecognizer);
                     Image image = new Image { Source = CollapseImage, WidthRequest = ImageWidth, HeightRequest = ImageHeight };
+
                     Label TitleLabel = new Label { Text = item.Title, TextColor = TitleTextColor, FontAttributes = TitleFontAttributes, FontFamily = TitleFontFamily, FontSize = TitleFontSize, LineBreakMode = LineBreakMode.TailTruncation };
                     titleBar.Children.Add(image);
                     titleBar.Children.Add(TitleLabel);
-                    StackLayout ContentLayout = new StackLayout { StyleId = "content_" + itemIndex.ToString(), Padding = ContentPadding, IsVisible = item.Expanded, HorizontalOptions = LayoutOptions.FillAndExpand };
-                    if (ExpandMode != ExpandModeEnum.UserDefined)
-                    {
-                        switch (ExpandMode)
-                        {
-                            case ExpandModeEnum.CollapseAll:
-                                ContentLayout.IsVisible = false;
-                                break;
-                            case ExpandModeEnum.ExpandAll:
-                                ContentLayout.IsVisible = true;
-                                break;
+                    StackLayout ContentLayout = new StackLayout { StyleId = "content_" + itemIndex.ToString(), Padding = ContentPadding, HorizontalOptions = LayoutOptions.FillAndExpand };
+                    ContentLayout.SizeChanged += ContentLayout_SizeChanged;
 
-                        }
-                    }
                     ContentLayout.Children.Add(item.Content);
 
 
                     subContainer.Children.Add(titleBar);
                     subContainer.Children.Add(ContentLayout);
                     mainContainer.Children.Add(subContainer);
+
+                    item._Key = itemIndex.ToString();
+                    item._TitleIcon = image;
+                    item._ContentLayout = ContentLayout;
+                    item._TitleBarLayout = titleBar;
+
                     itemIndex += 1;
                 }
             }
             this.Content = mainContainer;
         }
 
+        private void ContentLayout_SizeChanged(object sender, EventArgs e)
+        {
+            if (Animating == false)
+            {
+                int itemIndex = Convert.ToInt32(((StackLayout)sender).StyleId.Split('_')[1]);
+                ExpandableItems[itemIndex].ItemHeight = ((StackLayout)sender).Height;
+
+                switch (ExpandMode)
+                {
+                    case ExpandModeEnum.CollapseAll:
+                        ((StackLayout)sender).IsVisible = false;
+                        break;
+                    case ExpandModeEnum.ExpandAll:
+                        ((StackLayout)sender).IsVisible = true;
+                        break;
+                    case ExpandModeEnum.UserDefined:
+                        if (ExpandableItems[itemIndex].Expanded == false)
+                        {
+                            ((StackLayout)sender).IsVisible = false;
+                        }
+                        else
+                        {
+                            ((StackLayout)sender).IsVisible = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+               
+
+            }
+
+        }
+
         private void TitleBar_TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
             try
             {
+                if (Animating)
+                {
+                    return;
+                }
                 StackLayout subContainer = (StackLayout)((StackLayout)sender).Parent;
                 int itemIndex = Convert.ToInt32(((StackLayout)sender).StyleId.Split('_')[1]);
                 bool ContentVisible = false;
-                foreach (var item in subContainer.Children)
+
+
+
+                if (AutoCollapseInactiveItems)
                 {
-                    if (item.StyleId == "content_" + itemIndex.ToString())
+                    foreach (var item in ExpandableItems)
                     {
-                        item.IsVisible = !item.IsVisible;
-                        ContentVisible = item.IsVisible;
-                        ExpandableItems[itemIndex].Expanded = item.IsVisible;
-                        break;
-                    }
-
-                }
-
-                foreach (var item in subContainer.Children)
-                {
-
-                    if (item.StyleId == "titlebar_" + itemIndex.ToString())
-                    {
-                        StackLayout titleBar = (StackLayout)item;
-                        foreach (var titleview in titleBar.Children)
+                        if (Convert.ToInt32(item._Key) != itemIndex)
                         {
-                            if (titleview is Image img)
-                            {
-                                if (ContentVisible)
-                                {
-                                    img.Source = ExpandImage;
-                                }
-                                else
-                                {
-                                    img.Source = CollapseImage;
-                                }
-                                break;
-                            }
+                            item._ContentLayout.IsVisible = false;
+                            item._TitleIcon.Source = CollapseImage;
+                            item.Expanded = true;
                         }
-
                     }
                 }
 
-                if (DisplayOnlySingleContent)
+
+                ExpandableItem expandableItem = ExpandableItems.Find(f => f._Key == itemIndex.ToString());
+                ContentVisible = !expandableItem._ContentLayout.IsVisible;
+
+
+                if (ContentVisible)
                 {
-                    foreach (var item in mainContainer.Children)
-                    {
-                        if(item is StackLayout subcont)
-                        {
-                            foreach (StackLayout sl in subcont.Children)
-                            {
-                                if (sl.StyleId.Contains("content_"))
-                                {
-                                    if(sl.StyleId!= "content_" + itemIndex.ToString())
-                                    {
-                                        sl.IsVisible = false;
-                                        ExpandableItems[Convert.ToInt32(sl.StyleId.Split('_')[1])].Expanded = item.IsVisible;
-                                    }
-                                }
-                            }
-                        }
+                    ShowExpandAnimation(expandableItem._ContentLayout);
 
-                    }
+
                 }
+                else
+                {
+                    ShowCollapseAnimation(expandableItem._ContentLayout);
+
+
+                }
+
+                expandableItem.Expanded = ContentVisible;
+
+
+
+                if (expandableItem.Expanded)
+                {
+                    expandableItem._TitleIcon.Source = ExpandImage;
+                }
+                else
+                {
+                    expandableItem._TitleIcon.Source = CollapseImage;
+                }
+
+              
+
+
 
             }
             catch (Exception ex)
@@ -148,6 +184,87 @@ namespace RSPLMarketSurvey.CustomControls
             }
         }
 
+        private void ShowCollapseAnimation(View item)
+        {
+            Animating = true;
+            int itemIndex = Convert.ToInt32(((StackLayout)item).StyleId.Split('_')[1]);
+            switch (CollapseAnimation)
+            {
+                case AnimationEnum.None:
+                    Animating = false;
+                    break;
+                case AnimationEnum.Resize:
+
+                    double ContentHeight = ExpandableItems[itemIndex].ItemHeight;
+
+                    double ResizeSpeed = ContentHeight / 15.00;
+                    Device.StartTimer(TimeSpan.FromMilliseconds(1), () =>
+                    {
+                        if (item.HeightRequest <= 0)
+                        {
+                            Animating = false;
+                            return false;
+                        }
+                        else
+                        {
+                            item.HeightRequest -= ResizeSpeed;
+                            return true;
+                        }
+                    }
+
+                    );
+                    break;
+                default:
+                    break;
+            }
+
+            item.IsVisible = false;
+
+        }
+
+        private void ShowExpandAnimation(View item)
+        {
+            Animating = true;
+            int itemIndex = Convert.ToInt32(((StackLayout)item).StyleId.Split('_')[1]);
+            switch (ExpandAnimation)
+            {
+                case AnimationEnum.None:
+                   
+                    item.IsVisible = true;
+                    Animating = false;
+                    //await item.FadeTo(1);
+                    break;
+                case AnimationEnum.Resize:
+                    double ContentHeight = ExpandableItems[itemIndex].ItemHeight;
+                    item.HeightRequest = 0;
+                    item.IsVisible = true;
+                    double ResizeSpeed = ContentHeight / 15.00;
+                    Device.StartTimer(TimeSpan.FromMilliseconds(1), () =>
+                    {
+                        if (item.Height >= ContentHeight)
+                        {
+                            Animating = false;
+                            return false;
+                        }
+                        else
+                        {
+                            item.HeightRequest += ResizeSpeed;
+                            return true;
+                        }
+                    }
+
+                    );
+
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+
+        
         public enum ExpandModeEnum
         {
 
@@ -155,9 +272,11 @@ namespace RSPLMarketSurvey.CustomControls
             ExpandAll = 1,
             UserDefined = 2
         }
+
+     
         public ExpandModeEnum ExpandMode { get; set; }
 
-        public bool DisplayOnlySingleContent { get => _displayOnlySingleContent; set => _displayOnlySingleContent = value; }
+        public bool AutoCollapseInactiveItems { get => _displayOnlySingleContent; set => _displayOnlySingleContent = value; }
 
         public string Title { get; set; }
         public ImageSource CollapseImage { get; set; }
@@ -173,14 +292,23 @@ namespace RSPLMarketSurvey.CustomControls
         public double TitleFontSize { get => _titleFontSize; set => _titleFontSize = value; }
         public Color TitleTextColor { get => _titleTextColor; set => _titleTextColor = value; }
         public Thickness ContentPadding { get => _contentPadding; set => _contentPadding = value; }
+
+        public AnimationEnum ExpandAnimation { get; set; }
+        public AnimationEnum CollapseAnimation { get; set; }
+        private View ContentToAnimate { get; set; }
+        private bool Animating { get => _animating; set => _animating = value; }
     }
 
     public class ExpandableItem
     {
-        private bool _Expanded=false;
-
+        private bool _Expanded = false;
+        internal string _Key;
         public string Title { get; set; }
+        internal Image _TitleIcon { get; set; }
+        internal StackLayout _ContentLayout { get; set; }
+        internal StackLayout _TitleBarLayout { get; set; }
         public View Content { get; set; }
+        public double ItemHeight { get; set; }
         public bool Expanded { get => _Expanded; set => _Expanded = value; }
     }
 }
