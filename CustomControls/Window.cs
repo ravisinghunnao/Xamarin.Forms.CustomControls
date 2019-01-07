085be5551a87ba3c3f7f94ac0c424ec1b5d1a49b
Ravi Singh <rv_eps@hotmail.com>
2019-01-07 17:10:44 +0530

    added bindable properties

--- CustomControls/Window.cs
+++ CustomControls/Window.cs
@@ -12,13 +12,13 @@ namespace RSPLMarketSurvey.CustomControls
 	public class Window : ContentView
 	{
        
-        private Thickness _contentPadding = 10;
+        private static Thickness _contentPadding = 10;
        
-        private FontAttributes _titleFontAttributes = FontAttributes.Bold;
-        private double _titleFontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
-        private Color _titleTextColor = Color.White;
-        private Color _titlebarBackgroundColor = Color.FromHex("#0066cc");
-        private double _titlebarHeight = 50;
+        private static FontAttributes _titleFontAttributes = FontAttributes.Bold;
+        private static double _titleFontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+        private static Color _titleTextColor = Color.White;
+        private static Color _titlebarBackgroundColor = Color.FromHex("#0066cc");
+        private static double _titlebarHeight = 50;
         private string _defaultIconBase64 = "iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAGLgAABi4Bu5kyRgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAXzSURBVHic7ZxbqBVVGMf/yyQzwkwDj1p5LNPMHiy6YF6yo5685QUigpAIgoguGKlBERo9BBVdIKOnrBcherAHMYzyQegqhBmVqVl6VEhNM48nzTy/HmbUce01+3L2ntl7n/P94MBh5j9rfbP++1uzZq2ZkQzDMAzDMAzDMAzDMAzDMAzDMAzDMAzDMCQBo+sdgxEDTAVOAG8Art7x9GmACcBhzvNyvWPqswDjgYMU8qZlSs4ArUBHwAwzJW+AFmBnETMazpSGCKKnAPdLKnYtuFzSUG9bt6R+Ae0eSWdSytnjnGurPMLK6Z9HJRkySNK1FeiPSGqT9JikR719o4oc111hXD0m9EvprRyXNNc5970iQ96tczxB+oohpyTd55z7RpKcc0h6QtLaukYVoNm7LJ9uSc8Ftm9xzm1KbnDOnQGWSNosabCnb1fUtRmVADzijZZOl9BPBB4vo9yXvHJ31i7q4vS2DEkFmCxpvaTBwEDn3Gv1jilEs19DLi5HFJvxic53Ta8Cyyqpp1HuUxoW4Gbgr1JdFtBGNKEYYmVK2X6XBfAe0Ow/4GwARgP7Ao2219NNAo6lmHGW5YHyn0rRvoVlyoUAI4BdgcY6AFyf0N1RhhlBU4B+wPsp2tfzP+sGBRgK/BBopEPAhITudgq7M4D9wPqUhl7h1dUPWJOibcgBQa4AQ4BtgcY5DNyU0N0FdAZ0u4FRwEXA2pSGXunV6YC3U7R9u/ui8J7jLBuB/rHmVuBoQLMfGJcoa0B8XIhlXr1TgP8Cuk5gbN7t0FAQHv0AfEh0zTiSYsZYr5wRwC8B7UlgbkI3DTge0B0Ebsu/BRoQYFWKKWeqNOMfYE5CN4PwcLkDuCH/M29ggBdSTKnGjNkJXXu8zWcn0Jr7CTcDwNNFzNgNXOPpRwO/B7THgameNtQ1fg0MyfcsmwhgHnC6gszYnpIZ9wTK9g05ZGYUAVgM/FtlZvwNTEkpv26zvU1H1mbEx5kh5QAsqpEZk0vUY4aUAphNeOSzj8QcVqwdTviacQKYkdCNJ7rmlMIMSVIjM7qB9oRuIvBHGWaYIUmAhcCpQCP9SmE31Uq4mzrLBqJpkzbCd+BmSDFIv0GrJDN8NhJdRyrB1tSBWZI+lnSJt2ufpLudc7sS2uGSNkka52lDTym2q5ADkp5V9LhQiM4yw+6dAAuovpvqAmYBK0r8+nf4ZRoJamlGQrcsxYxtQEv+Z9kkAPfWwIxOYFqg7Gc83bfYdEg6pD8d0gGM8bTDgZ8D2gvWMwJ1PB/rtgBXZn9WTUoNzZiTVkfi+CXAoOzOpskB5hPupnZRfjdVMIVu9IASZlztac2MLAGmE346ZC9wnadtSemmgusZ3rEDgCsSf/6T7gbR4tLJCjLjt5TMSJ1CTxy/1DvuaHZn1oQAM4nuE6oxo+h6hleGGZIG0ZcUQpN6ad3UTwHtBTd9RepaTfRYkD966463H6Ev36HX0IyZZda3JnC8T2smJ1sFuUwuEnUvGyRd5u3qkDTdObc7oW1RNFE43tN2SVronPssy1jjGO6U9KK3ebFzrvknGUl/4Cx0zbiK8Iv+JZddA/X2OEOIlop9chmdZZohcSOuk3Spt2uvpHbnXEdC2yLpU0ljPG2XpEXOuS8qrP4jSdslTZE0P7H9pKRV8f995wJP+nRIJZlxjKj7qCaOikdZvS5D4kYsNzOGKZwZJxRlxpdVhrNDUbYkyw3FOzKxKfQg9SIgeexW51zjL+0SLQqVmxkji2TGpBxjXheIoRRLs4gli5cYH1BhZkjS5xVkxgLn3FcZxNb3IHpD6YOUX9UrsWYY8GNgf3BxKYeYGyZDan4NiT9Z8bAkJD3k7V4ODFT02YobvX2dkuY55zbXOiZD516aTMsUn4aaQqeOo6xMIXppcnUJM/4Ebql3rEl6rSHSOVPeaRYzpF5uiJRqSkOaIfXCG0Mf5xzAk4o+yfegzg9tv8uj/h6wVYWfAOyqRyCZQjQkXkMZ6xmGYRiGYRiGYRiGYRiGYRiGYRiGYRiGYRhJ/gcCAgFGIMaegQAAAABJRU5ErkJggg==";
         private ImageSource _titleIcon;
         public Window ()
@@ -128,24 +128,37 @@ namespace RSPLMarketSurvey.CustomControls
         public ImageSource TitleIcon { get => _titleIcon; set => _titleIcon = value; }
         public string TitleText { get; set; }
         public FontAttributes TitleFontAttributes { get => _titleFontAttributes; set => _titleFontAttributes = value; }
+        public static readonly BindableProperty TitleFontAttributesProperty = BindableProperty.Create("TitleFontAttributes", typeof(FontAttributes), typeof(FontAttributes), _titleFontAttributes);
+
         public string TitleFontFamily { get; set; }
+        public static readonly BindableProperty TitleFontFamilyProperty = BindableProperty.Create("TitleFontFamily", typeof(string), typeof(string), "Arial");
+
         public double TitleFontSize { get => _titleFontSize; set => _titleFontSize = value; }
+        public static readonly BindableProperty TitleFontSizeProperty = BindableProperty.Create("TitleFontSize", typeof(double), typeof(double), _titleFontSize);
+
         public Color TitleTextColor { get => _titleTextColor; set => _titleTextColor = value; }
+        public static readonly BindableProperty TitleTextColorProperty = BindableProperty.Create("TitleTextColor", typeof(Color), typeof(Color), _titleTextColor);
+
         public Color ContentBackgroundColor { get; set; }
-      
-    
+        public static readonly BindableProperty ContentBackgroundColorProperty = BindableProperty.Create("ContentBackgroundColor", typeof(Color), typeof(Color), Color.Transparent);
+
         public Thickness ContentPadding { get => _contentPadding; set => _contentPadding = value; }
-      
+        public static readonly BindableProperty ContentPaddingProperty = BindableProperty.Create("ContentPadding", typeof(Thickness), typeof(Thickness), _contentPadding);
+
 
         private StackLayout ContentLayout { get; set; }
         private Image _IconImage { get; set; }
         private Label _TitleLabel { get; set; }
       
         public Color TitlebarBackgroundColor { get => _titlebarBackgroundColor; set => _titlebarBackgroundColor = value; }
-   
+        public static readonly BindableProperty TitlebarBackgroundColorProperty = BindableProperty.Create("TitlebarBackgroundColor", typeof(Color), typeof(Color), _titlebarBackgroundColor);
+
         public ImageSource TitileBackgroundImageSource { get; set; }
+        public static readonly BindableProperty TitileBackgroundImageSourceProperty = BindableProperty.Create("TitileBackgroundImageSource", typeof(ImageSource), typeof(ImageSource), null);
         public double TitlebarHeight { get => _titlebarHeight; set => _titlebarHeight = value; }
+        public static readonly BindableProperty TitlebarHeightProperty = BindableProperty.Create("TitlebarHeight", typeof(double), typeof(double), _titlebarHeight);
         public ImageSource ContentBackgroundImageSource { get; set; }
+        public static readonly BindableProperty ContentBackgroundImageSourceProperty = BindableProperty.Create("ContentBackgroundImageSource", typeof(ImageSource), typeof(ImageSource), null);
         public View ContentBackgroundImage { get; private set; }
         public StackLayout CommandContainer { get; private set; }
         public StackLayout TitlebarLayout { get; private set; }
--- CustomControls/Wizard.cs
+++ CustomControls/Wizard.cs
@@ -11,26 +11,26 @@ namespace RSPLMarketSurvey.CustomControls
 {
     public class Wizard : ContentView
     {
-        private string _firstButtonText = "⇤";
-        private string _previousButtonText = "↤";
-        private string _nextButtonText = "↦";
-        private string _lastButtonText = "⇥";
+        private static string _firstButtonText = "⇤";
+        private static string _previousButtonText = "↤";
+        private static string _nextButtonText = "↦";
+        private static string _lastButtonText = "⇥";
 
-        private Thickness _contentPadding = 10;
+        private static Thickness _contentPadding = 10;
         private int _activeIndex = 0;
-        private FontAttributes _titleFontAttributes = FontAttributes.Bold;
-        private double _titleFontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
-        private Color _titleTextColor = Color.White;
-        private Color _titlebarBackgroundColor = Color.FromHex("#0066cc");
-        private double _titlebarHeight = 50;
-        private string _defaultIconBase64 = "iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAGLgAABi4Bu5kyRgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAfrSURBVHic7Z1brB1VGcf/H6jc2tPSOwekLYKX+CCCGi5NLFC1Cg+8+GaMLz4QEh+JLyY1MUGNiQkBH2miRBPjg1esLXhaW4RDL7S0nNJWes4xgCUVWhQkGvHnw5pNT6cza62999z2nvklTZqz1zfzzfrv71sza32zttTR0dHR0dHR0dHR0dFRKcAG4DHg/XX70nqATwH/wLENuLRun1oLcD3wKufzS+ADdfvWOoDrgJfJ5g/AZXX72BqAtcB8jhidKFUCrAGOB8Tosb0TpUSA5cDzkWL02AVcUaWfVuXJigSYkLQisvnFkh6VtGGAUz0u6RuSiGw/b2bvDnAeSaMtyP2SHq7bjwxWmdnpQY0vKtKTEWWnpFsk/b1mPyR1guyRdI+ZTUvaJOn1mv1ptSAH5MR4W5LM7JAaIEpbBZmRtNnM3lz4RzM7qJpFaaMgp+QiI3PgTUS5WzWJ8r46TloyJyRt9nx+1sze8B3AzKaBGyRdmdPkBknbBvTPyzgK8h8zOxnTEFgs6XuSvmVm50WEmZ2RdCbHrrQn+DamLEkSsFLSLkn3SdoDXFWzS5JaKgiwQtIOSZ9M/vRRSY8Dy+vzyjHKglw8iFHS6U9I+kTqoxslPVGAKAP5NdIAHwdO50wIHvHYrQGOBiYUj4bSV3L+PP4ELCr+qhsKbnHpFU+HZAoSKUaUKAFB2iMKcesZj2XYXQnsixSjx3PkpC9gBfBawP53wCXl90pNAMsIr2dsT3cCsBTY26cYMaJ8GjgbsP/tWIoCTADTgYvfTWoxKRHj2QHFiBHlVuDNVokCXAbsDFz0XtyC1UK7JYRFnAduAWaGEOU2zpUSjbcowKXAkxFiLEnZLU860cdfgA8m7VcCRwLtcwd64Cbg9YD96K/RAz8OXOQZYE3KZgL4c8BuHrguZXcN8FLAzhcpmwO2AD8vs79KB9gIvB24yB8ClrRfDDwVaH+BGAvOdyPwv4D9rzPslkac9yzw2bL7rHSAOyJEeSgRY0+EGOtzzhNza5wVWSuA/QG7V4H0zMDoAtwZIcqpGsS4CnghYDcLfKianqqQSFHymCtAjPUpu3XAiYDdUeCaanqoBoC7gH9VLMYF9sAqLizUTvMcsKqanqkR4HbC9/09TpDzDcXl/kOD2BOey9pBxRWPtQJ8NUKMUGSEplN89j5BnqQNE4s9cM8Lodzt68yYua05YJ3HB58gjVh5rATc1HvoVYJS0lTqOD5BVpZz9Q2jIDEODitGcqx2C1KRGDOkpmE8/rRXkKaJkRyzNEEaXZcFXC1XkHatp9m8pM+Z2csZ9kvl3u/wTVvMSfqimZ1aYHeJpJ9IujnHpn0vhgJXE16uzb0bIm5x6iSwNmV3Oe4dw0EZv5QVKcbskGK8lbbHTVDuGkKM8RMEWE/5Y0aPR4GLEruVhBe1YhgfQahWjB5bgWuJLxEKMR6CMHyaillDzyNUqNAPo3+XBUxKmpIr889jTtIdZjaXYb9E0nZJnxnQhQnPZ3+T9B1J/4081j8H9KEZAJPAscC3bhZ/ZDwTYf8V3EDeDycZx8WlPIgbM3If2oirLnmhZ09cHVWPg8DqanukRioUY3XKLqaO6incQ2U7SMSYq1qMBfY+UbbTssWl2DFjbY59zJjxEuCbbslbo98GXF7OlTcQqomMI0Tmfs5fDv4FbdrmDxcZLwY684K5pQX2S4CnA/bvlYf24dcm4Ke0aSc54iLjvbuhDPtCI6PV9CFG3gAcK8b4l9wMC66YrGwxDndiRIArsxxmzIipYj+OW8QaG0qZy8KVwvxR0kc8zWbl5qbmM+wn5FYKb/XYH5d0p5m90odfGyV9OOtYZrYz9jgjBcWkqQMB++cZIE3hptqz2Dr8lRdDoRGCmwDcKSkzDSXMyH2zX8uwX67zd1jI4rCku4bZRq/JFCZIkqZ+L78YJ+UKCrLEmJD0G/nFOCa3z1W0GMDNOrerz2ROs0lgU/L/M2a2P/b4jaSANLUsMk31vfgDTAWOm2Zq+B6pkSaLkRx/pAQZKmXhnqyHSVOLFU5TL0r6fNPGDNzkZVb/nTaz6lcNk8iYLTkyDg0aGQvOU0qEeK79a8P4O9D2TLi7qSlJ6zzNfHdTy+S2SPJFxn5JG5sWGWXTtyC4EpwdKleMfXLloZlb7HUk4HbjCW1Dkbs4RNyry7lprgD/t+acs+8Hw9pTVhIZuyV9zNNsRtJtZvbXDPtlcg+Nt3vs90nakBVZbSHqLgs3gTcl6XpPs1Ca2iHpJo99L02djfGptQCrCb8cn7tSl6Sp3QH7SqbQgStwL3ym/2UWNeBKTN/I+fduzrW8ldP+kSIuYJJweWfo1ja07cSzNLTkBndrXxRR41RoDPm6/OWdkvSDnDS1SNKv5E9ThyXd3aWpSAADvh9Q/t/AvSm7mMiYpqGR0YMaIiTWse8GTvYOsDlpuwi3K6ePoZ/Aq4CmCpI492DghO8AXyZOjNjfjqoVahCkr9+gAh6U9M2Brs5xSNImM2vEzwuFwN19fSnn40ckZUX5j+Set9LMmdneglw7B7BlwG/IM6T2RhxlqPtJvYeZbZH07T7NpiV9If2LNh0F0kekjFVk9CgrQoZ1KiTK0+MohtRQQRLH8kQZWzGk8gQZuurEzLbgNgh+YMGfD8o9gY/zmPEzSVl7+B6r2pFMFkTKARr+BN4agAcYs1rbjo6Ojo6Ojo6Ojo7C+T/1i2mVl5+2SAAAAABJRU5ErkJggg==";
-        private ImageSource _titleIcon;
-        private Thickness _footerPadding = 10;
-        private Color _footerBackgroundColor = Color.FromHex("#0066cc");
-        private double _buttonFontSize=Device.GetNamedSize(NamedSize.Large,typeof(Button));
-        private Color _buttonTextColor= Color.FromHex("#0066cc");
-        private Color _buttonBackgroundColor=Color.White;
-        private int _buttonCornerRadius=25;
+        private static FontAttributes _titleFontAttributes = FontAttributes.Bold;
+        private static double _titleFontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+        private static Color _titleTextColor = Color.White;
+        private static Color _titlebarBackgroundColor = Color.FromHex("#0066cc");
+        private static double _titlebarHeight = 50;
+        private static string _defaultIconBase64 = "iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAGLgAABi4Bu5kyRgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAfrSURBVHic7Z1brB1VGcf/H6jc2tPSOwekLYKX+CCCGi5NLFC1Cg+8+GaMLz4QEh+JLyY1MUGNiQkBH2miRBPjg1esLXhaW4RDL7S0nNJWes4xgCUVWhQkGvHnw5pNT6cza62999z2nvklTZqz1zfzzfrv71sza32zttTR0dHR0dHR0dHR0dFRKcAG4DHg/XX70nqATwH/wLENuLRun1oLcD3wKufzS+ADdfvWOoDrgJfJ5g/AZXX72BqAtcB8jhidKFUCrAGOB8Tosb0TpUSA5cDzkWL02AVcUaWfVuXJigSYkLQisvnFkh6VtGGAUz0u6RuSiGw/b2bvDnAeSaMtyP2SHq7bjwxWmdnpQY0vKtKTEWWnpFsk/b1mPyR1guyRdI+ZTUvaJOn1mv1ptSAH5MR4W5LM7JAaIEpbBZmRtNnM3lz4RzM7qJpFaaMgp+QiI3PgTUS5WzWJ8r46TloyJyRt9nx+1sze8B3AzKaBGyRdmdPkBknbBvTPyzgK8h8zOxnTEFgs6XuSvmVm50WEmZ2RdCbHrrQn+DamLEkSsFLSLkn3SdoDXFWzS5JaKgiwQtIOSZ9M/vRRSY8Dy+vzyjHKglw8iFHS6U9I+kTqoxslPVGAKAP5NdIAHwdO50wIHvHYrQGOBiYUj4bSV3L+PP4ELCr+qhsKbnHpFU+HZAoSKUaUKAFB2iMKcesZj2XYXQnsixSjx3PkpC9gBfBawP53wCXl90pNAMsIr2dsT3cCsBTY26cYMaJ8GjgbsP/tWIoCTADTgYvfTWoxKRHj2QHFiBHlVuDNVokCXAbsDFz0XtyC1UK7JYRFnAduAWaGEOU2zpUSjbcowKXAkxFiLEnZLU860cdfgA8m7VcCRwLtcwd64Cbg9YD96K/RAz8OXOQZYE3KZgL4c8BuHrguZXcN8FLAzhcpmwO2AD8vs79KB9gIvB24yB8ClrRfDDwVaH+BGAvOdyPwv4D9rzPslkac9yzw2bL7rHSAOyJEeSgRY0+EGOtzzhNza5wVWSuA/QG7V4H0zMDoAtwZIcqpGsS4CnghYDcLfKianqqQSFHymCtAjPUpu3XAiYDdUeCaanqoBoC7gH9VLMYF9sAqLizUTvMcsKqanqkR4HbC9/09TpDzDcXl/kOD2BOey9pBxRWPtQJ8NUKMUGSEplN89j5BnqQNE4s9cM8Lodzt68yYua05YJ3HB58gjVh5rATc1HvoVYJS0lTqOD5BVpZz9Q2jIDEODitGcqx2C1KRGDOkpmE8/rRXkKaJkRyzNEEaXZcFXC1XkHatp9m8pM+Z2csZ9kvl3u/wTVvMSfqimZ1aYHeJpJ9IujnHpn0vhgJXE16uzb0bIm5x6iSwNmV3Oe4dw0EZv5QVKcbskGK8lbbHTVDuGkKM8RMEWE/5Y0aPR4GLEruVhBe1YhgfQahWjB5bgWuJLxEKMR6CMHyaillDzyNUqNAPo3+XBUxKmpIr889jTtIdZjaXYb9E0nZJnxnQhQnPZ3+T9B1J/4081j8H9KEZAJPAscC3bhZ/ZDwTYf8V3EDeDycZx8WlPIgbM3If2oirLnmhZ09cHVWPg8DqanukRioUY3XKLqaO6incQ2U7SMSYq1qMBfY+UbbTssWl2DFjbY59zJjxEuCbbslbo98GXF7OlTcQqomMI0Tmfs5fDv4FbdrmDxcZLwY684K5pQX2S4CnA/bvlYf24dcm4Ke0aSc54iLjvbuhDPtCI6PV9CFG3gAcK8b4l9wMC66YrGwxDndiRIArsxxmzIipYj+OW8QaG0qZy8KVwvxR0kc8zWbl5qbmM+wn5FYKb/XYH5d0p5m90odfGyV9OOtYZrYz9jgjBcWkqQMB++cZIE3hptqz2Dr8lRdDoRGCmwDcKSkzDSXMyH2zX8uwX67zd1jI4rCku4bZRq/JFCZIkqZ+L78YJ+UKCrLEmJD0G/nFOCa3z1W0GMDNOrerz2ROs0lgU/L/M2a2P/b4jaSANLUsMk31vfgDTAWOm2Zq+B6pkSaLkRx/pAQZKmXhnqyHSVOLFU5TL0r6fNPGDNzkZVb/nTaz6lcNk8iYLTkyDg0aGQvOU0qEeK79a8P4O9D2TLi7qSlJ6zzNfHdTy+S2SPJFxn5JG5sWGWXTtyC4EpwdKleMfXLloZlb7HUk4HbjCW1Dkbs4RNyry7lprgD/t+acs+8Hw9pTVhIZuyV9zNNsRtJtZvbXDPtlcg+Nt3vs90nakBVZbSHqLgs3gTcl6XpPs1Ca2iHpJo99L02djfGptQCrCb8cn7tSl6Sp3QH7SqbQgStwL3ym/2UWNeBKTN/I+fduzrW8ldP+kSIuYJJweWfo1ja07cSzNLTkBndrXxRR41RoDPm6/OWdkvSDnDS1SNKv5E9ThyXd3aWpSAADvh9Q/t/AvSm7mMiYpqGR0YMaIiTWse8GTvYOsDlpuwi3K6ePoZ/Aq4CmCpI492DghO8AXyZOjNjfjqoVahCkr9+gAh6U9M2Brs5xSNImM2vEzwuFwN19fSnn40ckZUX5j+Set9LMmdneglw7B7BlwG/IM6T2RhxlqPtJvYeZbZH07T7NpiV9If2LNh0F0kekjFVk9CgrQoZ1KiTK0+MohtRQQRLH8kQZWzGk8gQZuurEzLbgNgh+YMGfD8o9gY/zmPEzSVl7+B6r2pFMFkTKARr+BN4agAcYs1rbjo6Ojo6Ojo6Ojo7C+T/1i2mVl5+2SAAAAABJRU5ErkJggg==";
+        private static ImageSource _titleIcon;
+        private static Thickness _footerPadding = 10;
+        private static Color _footerBackgroundColor = Color.FromHex("#0066cc");
+        private static double _buttonFontSize=Device.GetNamedSize(NamedSize.Large,typeof(Button));
+        private static Color _buttonTextColor= Color.FromHex("#0066cc");
+        private static Color _buttonBackgroundColor=Color.White;
+        private static int _buttonCornerRadius=25;
 
         public Wizard()
         {
@@ -266,16 +266,37 @@ namespace RSPLMarketSurvey.CustomControls
         public ImageSource TitleIcon { get => _titleIcon; set => _titleIcon = value; }
         public string TitleText { get; set; }
         public FontAttributes TitleFontAttributes { get => _titleFontAttributes; set => _titleFontAttributes = value; }
+        public static readonly BindableProperty TitleFontAttributesProperty = BindableProperty.Create("TitleFontAttributes", typeof(FontAttributes), typeof(FontAttributes), _titleFontAttributes);
+
         public string TitleFontFamily { get; set; }
+        public static readonly BindableProperty TitleFontFamilyProperty = BindableProperty.Create("TitleFontFamily", typeof(string), typeof(string), "Arial");
+
         public double TitleFontSize { get => _titleFontSize; set => _titleFontSize = value; }
+        public static readonly BindableProperty TitleFontSizeProperty = BindableProperty.Create("TitleFontSize", typeof(double), typeof(double), _titleFontSize);
+
         public Color TitleTextColor { get => _titleTextColor; set => _titleTextColor = value; }
+        public static readonly BindableProperty TitleTextColorProperty = BindableProperty.Create("TitleTextColor", typeof(Color), typeof(Color), _titleTextColor);
+
         public Color ContentBackgroundColor { get; set; }
+        public static readonly BindableProperty ContentBackgroundColorProperty = BindableProperty.Create("ContentBackgroundColor", typeof(Color), typeof(Color),Color.Transparent);
+
         public string FirstButtonText { get => _firstButtonText; set => _firstButtonText = value; }
+        public static readonly BindableProperty FirstButtonTextProperty = BindableProperty.Create("FirstButtonText", typeof(string), typeof(string), _firstButtonText);
+
         public string PreviousButtonText { get => _previousButtonText; set => _previousButtonText = value; }
+        public static readonly BindableProperty PreviousButtonTextProperty = BindableProperty.Create("PreviousButtonText", typeof(string), typeof(string), _previousButtonText);
+
         public string NextButtonText { get => _nextButtonText; set => _nextButtonText = value; }
+        public static readonly BindableProperty NextButtonTextProperty = BindableProperty.Create("NextButtonText", typeof(string), typeof(string), _nextButtonText);
+
         public string LastButtonText { get => _lastButtonText; set => _lastButtonText = value; }
+        public static readonly BindableProperty LastButtonTextProperty = BindableProperty.Create("LastButtonText", typeof(string), typeof(string), _lastButtonText);
+
         public List<WizardStep> WizardSteps { get; set; }
         public Thickness ContentPadding { get => _contentPadding; set => _contentPadding = value; }
+        public static readonly BindableProperty ContentPaddingProperty = BindableProperty.Create("ContentPadding", typeof(Thickness), typeof(Thickness), _contentPadding);
+
+
         public int ActiveIndex { get => _activeIndex; set => _activeIndex = value; }
 
         private StackLayout ContentLayout { get; set; }
@@ -286,16 +307,35 @@ namespace RSPLMarketSurvey.CustomControls
         public Button NextButton { get; private set; }
         public Button LastButton { get; private set; }
         public Color TitlebarBackgroundColor { get => _titlebarBackgroundColor; set => _titlebarBackgroundColor = value; }
+        public static readonly BindableProperty TitlebarBackgroundColorProperty = BindableProperty.Create("TitlebarBackgroundColor", typeof(Color), typeof(Color), _titlebarBackgroundColor);
+
         public Color FooterBackgroundColor { get => _footerBackgroundColor; set => _footerBackgroundColor = value; }
+        public static readonly BindableProperty FooterBackgroundColorProperty = BindableProperty.Create("FooterBackgroundColor", typeof(Color), typeof(Color), _footerBackgroundColor);
+
         public ImageSource TitileBackgroundImageSource { get; set; }
+        public static readonly BindableProperty TitileBackgroundImageSourceProperty = BindableProperty.Create("TitileBackgroundImageSource", typeof(ImageSource), typeof(ImageSource), null);
+
         public double TitlebarHeight { get => _titlebarHeight; set => _titlebarHeight = value; }
+        public static readonly BindableProperty TitlebarHeightProperty = BindableProperty.Create("TitlebarHeight", typeof(double), typeof(double), _titlebarHeight);
+
         public ImageSource ContentBackgroundImageSource { get; set; }
+        public static readonly BindableProperty ContentBackgroundImageSourceProperty = BindableProperty.Create("ContentBackgroundImageSource", typeof(ImageSource), typeof(ImageSource), null);
+
         public View ContentBackgroundImage { get; private set; }
         public Thickness FooterPadding { get => _footerPadding; set => _footerPadding = value; }
+        public static readonly BindableProperty FooterPaddingProperty = BindableProperty.Create("FooterPadding", typeof(Thickness), typeof(Thickness), _footerPadding);
+
         public double ButtonFontSize { get => _buttonFontSize; set => _buttonFontSize = value; }
+        public static readonly BindableProperty ButtonFontSizeProperty = BindableProperty.Create("ButtonFontSize", typeof(double), typeof(double), _buttonFontSize);
+
         public Color ButtonTextColor { get => _buttonTextColor; set => _buttonTextColor = value; }
+        public static readonly BindableProperty ButtonTextColorProperty = BindableProperty.Create("ButtonTextColor", typeof(Color), typeof(Color), _buttonTextColor);
+
         public Color ButtonBackgroundColor { get => _buttonBackgroundColor; set => _buttonBackgroundColor = value; }
+        public static readonly BindableProperty ButtonBackgroundColorProperty = BindableProperty.Create("ButtonBackgroundColor", typeof(Color), typeof(Color), _buttonBackgroundColor);
         public int ButtonCornerRadius { get => _buttonCornerRadius; set => _buttonCornerRadius = value; }
+        public static readonly BindableProperty ButtonCornerRadiusProperty = BindableProperty.Create("ButtonCornerRadius", typeof(int), typeof(int), _buttonCornerRadius);
+
     }
 
     public class WizardStep
