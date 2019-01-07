d1e9e3d3ec6fea9c10e148908ebeda4f43663350
Ravi Singh <rv_eps@hotmail.com>
2019-01-07 16:24:40 +0530

    Update Wizard.cs

--- CustomControls/Wizard.cs
+++ CustomControls/Wizard.cs
@@ -11,10 +11,11 @@ namespace RSPLMarketSurvey.CustomControls
 {
     public class Wizard : ContentView
     {
-        private string _firstButtonText = "⏮";
-        private string _previousButtonText = "⏪";
-        private string _nextButtonText = "⏩";
-        private string _lastButtonText = "⏭";
+        private string _firstButtonText = "⇤";
+        private string _previousButtonText = "↤";
+        private string _nextButtonText = "↦";
+        private string _lastButtonText = "⇥";
+
         private Thickness _contentPadding = 10;
         private int _activeIndex = 0;
         private FontAttributes _titleFontAttributes = FontAttributes.Bold;
@@ -24,6 +25,12 @@ namespace RSPLMarketSurvey.CustomControls
         private double _titlebarHeight = 50;
         private string _defaultIconBase64 = "iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAGLgAABi4Bu5kyRgAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAfrSURBVHic7Z1brB1VGcf/H6jc2tPSOwekLYKX+CCCGi5NLFC1Cg+8+GaMLz4QEh+JLyY1MUGNiQkBH2miRBPjg1esLXhaW4RDL7S0nNJWes4xgCUVWhQkGvHnw5pNT6cza62999z2nvklTZqz1zfzzfrv71sza32zttTR0dHR0dHR0dHR0dFRKcAG4DHg/XX70nqATwH/wLENuLRun1oLcD3wKufzS+ADdfvWOoDrgJfJ5g/AZXX72BqAtcB8jhidKFUCrAGOB8Tosb0TpUSA5cDzkWL02AVcUaWfVuXJigSYkLQisvnFkh6VtGGAUz0u6RuSiGw/b2bvDnAeSaMtyP2SHq7bjwxWmdnpQY0vKtKTEWWnpFsk/b1mPyR1guyRdI+ZTUvaJOn1mv1ptSAH5MR4W5LM7JAaIEpbBZmRtNnM3lz4RzM7qJpFaaMgp+QiI3PgTUS5WzWJ8r46TloyJyRt9nx+1sze8B3AzKaBGyRdmdPkBknbBvTPyzgK8h8zOxnTEFgs6XuSvmVm50WEmZ2RdCbHrrQn+DamLEkSsFLSLkn3SdoDXFWzS5JaKgiwQtIOSZ9M/vRRSY8Dy+vzyjHKglw8iFHS6U9I+kTqoxslPVGAKAP5NdIAHwdO50wIHvHYrQGOBiYUj4bSV3L+PP4ELCr+qhsKbnHpFU+HZAoSKUaUKAFB2iMKcesZj2XYXQnsixSjx3PkpC9gBfBawP53wCXl90pNAMsIr2dsT3cCsBTY26cYMaJ8GjgbsP/tWIoCTADTgYvfTWoxKRHj2QHFiBHlVuDNVokCXAbsDFz0XtyC1UK7JYRFnAduAWaGEOU2zpUSjbcowKXAkxFiLEnZLU860cdfgA8m7VcCRwLtcwd64Cbg9YD96K/RAz8OXOQZYE3KZgL4c8BuHrguZXcN8FLAzhcpmwO2AD8vs79KB9gIvB24yB8ClrRfDDwVaH+BGAvOdyPwv4D9rzPslkac9yzw2bL7rHSAOyJEeSgRY0+EGOtzzhNza5wVWSuA/QG7V4H0zMDoAtwZIcqpGsS4CnghYDcLfKianqqQSFHymCtAjPUpu3XAiYDdUeCaanqoBoC7gH9VLMYF9sAqLizUTvMcsKqanqkR4HbC9/09TpDzDcXl/kOD2BOey9pBxRWPtQJ8NUKMUGSEplN89j5BnqQNE4s9cM8Lodzt68yYua05YJ3HB58gjVh5rATc1HvoVYJS0lTqOD5BVpZz9Q2jIDEODitGcqx2C1KRGDOkpmE8/rRXkKaJkRyzNEEaXZcFXC1XkHatp9m8pM+Z2csZ9kvl3u/wTVvMSfqimZ1aYHeJpJ9IujnHpn0vhgJXE16uzb0bIm5x6iSwNmV3Oe4dw0EZv5QVKcbskGK8lbbHTVDuGkKM8RMEWE/5Y0aPR4GLEruVhBe1YhgfQahWjB5bgWuJLxEKMR6CMHyaillDzyNUqNAPo3+XBUxKmpIr889jTtIdZjaXYb9E0nZJnxnQhQnPZ3+T9B1J/4081j8H9KEZAJPAscC3bhZ/ZDwTYf8V3EDeDycZx8WlPIgbM3If2oirLnmhZ09cHVWPg8DqanukRioUY3XKLqaO6incQ2U7SMSYq1qMBfY+UbbTssWl2DFjbY59zJjxEuCbbslbo98GXF7OlTcQqomMI0Tmfs5fDv4FbdrmDxcZLwY684K5pQX2S4CnA/bvlYf24dcm4Ke0aSc54iLjvbuhDPtCI6PV9CFG3gAcK8b4l9wMC66YrGwxDndiRIArsxxmzIipYj+OW8QaG0qZy8KVwvxR0kc8zWbl5qbmM+wn5FYKb/XYH5d0p5m90odfGyV9OOtYZrYz9jgjBcWkqQMB++cZIE3hptqz2Dr8lRdDoRGCmwDcKSkzDSXMyH2zX8uwX67zd1jI4rCku4bZRq/JFCZIkqZ+L78YJ+UKCrLEmJD0G/nFOCa3z1W0GMDNOrerz2ROs0lgU/L/M2a2P/b4jaSANLUsMk31vfgDTAWOm2Zq+B6pkSaLkRx/pAQZKmXhnqyHSVOLFU5TL0r6fNPGDNzkZVb/nTaz6lcNk8iYLTkyDg0aGQvOU0qEeK79a8P4O9D2TLi7qSlJ6zzNfHdTy+S2SPJFxn5JG5sWGWXTtyC4EpwdKleMfXLloZlb7HUk4HbjCW1Dkbs4RNyry7lprgD/t+acs+8Hw9pTVhIZuyV9zNNsRtJtZvbXDPtlcg+Nt3vs90nakBVZbSHqLgs3gTcl6XpPs1Ca2iHpJo99L02djfGptQCrCb8cn7tSl6Sp3QH7SqbQgStwL3ym/2UWNeBKTN/I+fduzrW8ldP+kSIuYJJweWfo1ja07cSzNLTkBndrXxRR41RoDPm6/OWdkvSDnDS1SNKv5E9ThyXd3aWpSAADvh9Q/t/AvSm7mMiYpqGR0YMaIiTWse8GTvYOsDlpuwi3K6ePoZ/Aq4CmCpI492DghO8AXyZOjNjfjqoVahCkr9+gAh6U9M2Brs5xSNImM2vEzwuFwN19fSnn40ckZUX5j+Set9LMmdneglw7B7BlwG/IM6T2RhxlqPtJvYeZbZH07T7NpiV9If2LNh0F0kekjFVk9CgrQoZ1KiTK0+MohtRQQRLH8kQZWzGk8gQZuurEzLbgNgh+YMGfD8o9gY/zmPEzSVl7+B6r2pFMFkTKARr+BN4agAcYs1rbjo6Ojo6Ojo6Ojo7C+T/1i2mVl5+2SAAAAABJRU5ErkJggg==";
         private ImageSource _titleIcon;
+        private Thickness _footerPadding = 10;
+        private Color _footerBackgroundColor = Color.FromHex("#0066cc");
+        private double _buttonFontSize=Device.GetNamedSize(NamedSize.Large,typeof(Button));
+        private Color _buttonTextColor= Color.FromHex("#0066cc");
+        private Color _buttonBackgroundColor=Color.White;
+        private int _buttonCornerRadius=25;
 
         public Wizard()
         {
@@ -43,10 +50,10 @@ namespace RSPLMarketSurvey.CustomControls
 
         void Init()
         {
-            FirstButton = new Button { Text = FirstButtonText };
-            PreviousButton = new Button { Text = PreviousButtonText };
-            NextButton = new Button { Text = NextButtonText };
-            LastButton = new Button { Text = LastButtonText };
+            FirstButton = new Button { Text = FirstButtonText, FontSize = ButtonFontSize, TextColor = ButtonTextColor, BackgroundColor = ButtonBackgroundColor, CornerRadius = ButtonCornerRadius };
+            PreviousButton = new Button { Text = PreviousButtonText, FontSize = ButtonFontSize, TextColor = ButtonTextColor, BackgroundColor = ButtonBackgroundColor, CornerRadius = ButtonCornerRadius };
+            NextButton = new Button { Text = NextButtonText, FontSize = ButtonFontSize, TextColor = ButtonTextColor, BackgroundColor = ButtonBackgroundColor, CornerRadius = ButtonCornerRadius };
+            LastButton = new Button { Text = LastButtonText, FontSize = ButtonFontSize, TextColor = ButtonTextColor, BackgroundColor = ButtonBackgroundColor, CornerRadius = ButtonCornerRadius };
             _IconImage = new Image { Source = TitleIcon, Aspect = Aspect.AspectFit, Margin = new Thickness(5, 0, 0, 0) };
             _TitleLabel = new Label { Text = TitleText, FontAttributes = TitleFontAttributes, FontFamily = TitleFontFamily, FontSize = TitleFontSize, TextColor = TitleTextColor };
 
@@ -57,7 +64,7 @@ namespace RSPLMarketSurvey.CustomControls
                 Padding = ContentPadding,
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 BackgroundColor = ContentBackgroundColor,
-               
+
             };
 
             ContentLayout.SizeChanged += ContentLayout_SizeChanged;
@@ -116,7 +123,7 @@ namespace RSPLMarketSurvey.CustomControls
                                 {
                                     new Image {Source=TitileBackgroundImageSource,VerticalOptions=LayoutOptions.CenterAndExpand, HorizontalOptions=LayoutOptions.FillAndExpand,Aspect=Aspect.Fill},
                         new StackLayout{
-                                        VerticalOptions=LayoutOptions.CenterAndExpand,    
+                                        VerticalOptions=LayoutOptions.CenterAndExpand,
                                         HorizontalOptions = LayoutOptions.FillAndExpand,
                                         Orientation = StackOrientation.Horizontal,
                                         Children = {
@@ -149,6 +156,7 @@ namespace RSPLMarketSurvey.CustomControls
                     {
                         BackgroundColor=FooterBackgroundColor,
                         HorizontalOptions=LayoutOptions.FillAndExpand,
+                        Padding=FooterPadding,
                         Children =
                         {
                             FooterGrid
@@ -278,11 +286,16 @@ namespace RSPLMarketSurvey.CustomControls
         public Button NextButton { get; private set; }
         public Button LastButton { get; private set; }
         public Color TitlebarBackgroundColor { get => _titlebarBackgroundColor; set => _titlebarBackgroundColor = value; }
-        public Color FooterBackgroundColor { get; set; }
+        public Color FooterBackgroundColor { get => _footerBackgroundColor; set => _footerBackgroundColor = value; }
         public ImageSource TitileBackgroundImageSource { get; set; }
         public double TitlebarHeight { get => _titlebarHeight; set => _titlebarHeight = value; }
         public ImageSource ContentBackgroundImageSource { get; set; }
         public View ContentBackgroundImage { get; private set; }
+        public Thickness FooterPadding { get => _footerPadding; set => _footerPadding = value; }
+        public double ButtonFontSize { get => _buttonFontSize; set => _buttonFontSize = value; }
+        public Color ButtonTextColor { get => _buttonTextColor; set => _buttonTextColor = value; }
+        public Color ButtonBackgroundColor { get => _buttonBackgroundColor; set => _buttonBackgroundColor = value; }
+        public int ButtonCornerRadius { get => _buttonCornerRadius; set => _buttonCornerRadius = value; }
     }
 
     public class WizardStep
