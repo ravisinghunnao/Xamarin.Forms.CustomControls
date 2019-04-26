972f1c6791f447b866f422c15acd1a2ca914d13f
Ravi Singh <rv_eps@hotmail.com>
2019-04-26 15:08:08 +0530

    Update Repeater.cs

--- CustomControls/Repeater.cs
+++ CustomControls/Repeater.cs
@@ -22,6 +22,22 @@ namespace NIFShopping.CustomControls
 
         }
 
+        private View CreateClone(View view)
+        {
+            View ClonedView =(View) Activator.CreateInstance(view.GetType());
+            foreach (PropertyInfo propertyInfo in view.GetType().GetProperties())
+            {
+                if (propertyInfo.CanRead && propertyInfo.CanWrite)
+                {
+                    object PropertyValue = propertyInfo.GetValue(view, null);
+
+                    propertyInfo.SetValue(ClonedView, PropertyValue);
+
+                }
+            }
+            return ClonedView;
+        }
+
         protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             base.OnPropertyChanged(propertyName);
@@ -35,43 +51,47 @@ namespace NIFShopping.CustomControls
 
                             Content = null;
 
+
                             Layout<View> container = null;
 
-                            if (ContainerTemplate.Count <= 0)
+
+
+                            if (ContainerName == "")
                             {
-                                container= new StackLayout { Spacing = 0 };
+                                throw new Exception("ContainerName property is not set.");
+
                             }
                             else
                             {
-                                container = ContainerTemplate[0];
-                            }
+                                Layout<View> OriginalContainer = this.FindByName<Layout<View>>(ContainerName);
+                                container =(Layout<View>) CreateClone(OriginalContainer);
 
-                            
-                            
-                            foreach (var dataItem in ItemSource)
-                            {
-                                foreach (var item in DataTemplate)
+                              
+
+
+                                container.Children.Clear();
+
+                                foreach (var dataItem in ItemSource)
                                 {
 
-                                    item.BindingContext = dataItem;
-                                    View view = (View)Activator.CreateInstance(item.GetType());
-                                    foreach (PropertyInfo propertyInfo in item.GetType().GetProperties())
+                                    foreach (var item in OriginalContainer.Children)
                                     {
-                                        if (propertyInfo.CanRead && propertyInfo.CanWrite)
-                                        {
-                                            object PropertyValue = propertyInfo.GetValue(item, null);
-
-                                            propertyInfo.SetValue(view, PropertyValue);
-
-                                        }
+                                        View clonedview = CreateClone(item);
+                                        BindChildrens(item, clonedview, dataItem);
+                                        container.Children.Add(clonedview);
                                     }
+                                    
 
 
-                                    container.Children.Add(view);
 
+                            
                                 }
+                             
+                                Content = container;
+
                             }
-                            Content = container;
+
+
 
 
                         }
@@ -83,17 +103,54 @@ namespace NIFShopping.CustomControls
             catch (Exception ex)
             {
 
-                Application.Current.MainPage.DisplayAlert("Repeater Error", ex.Message, "OK");
+                throw ex;
             }
+
+        }
+
+
+
+        void BindChildrens(View OriginalView, View ClonedView, object BindingContext)
+        {
+         
+
+            if (OriginalView is ContentView contentView)
+            {
+                ((ContentView)ClonedView).Content = null;
+
+                var item = contentView.Content;
+                item.BindingContext = BindingContext;
+                View v = CreateClone(item);
+
+                
+                ((ContentView)ClonedView).Content = v;
+
+                BindChildrens(item, v,BindingContext);
+            }
+            else if (OriginalView is Layout<View> layout)
+            {
+                ((Layout<View>)ClonedView).Children.Clear();
+                foreach (var item in layout.Children)
+                {
+                    item.BindingContext = BindingContext;
+                    View v = CreateClone(item);
+                    
+                    ((Layout<View>)ClonedView).Children.Add(v);
+
+                    BindChildrens(item, v, BindingContext);
+                }
+            }
+            
+
 
         }
 
         public List<View> DataTemplate { get => _dataTemplate; set => _dataTemplate = value; }
 
-        public List<Layout<View>> ContainerTemplate { get => _container; set => _container = value; }
+        public string ContainerName { get => _containerName; set => _containerName = value; }
 
         public static BindableProperty ItemSourceProperty = BindableProperty.Create("ItemSource", typeof(IEnumerable), typeof(IEnumerable), null, BindingMode.TwoWay);
-        private List<Layout<View>> _container=new List<Layout<View>>();
+        private string _containerName = "";
 
         public IEnumerable ItemSource { get => (IEnumerable)GetValue(ItemSourceProperty); set { SetValue(ItemSourceProperty, value); } }
 
